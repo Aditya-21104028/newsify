e1a4ee7f49b7f262b54b910e25e04dd8f556fe19
Aditya Kumar <adityakumar.bt21ele@pec.edu.in>
2023-10-06 12:31:50 +0530

    FIrst commit

---
+++ src/components/News.js
@@ -0,0 +1,85 @@
+import React, { useEffect, useState } from 'react'
+import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
+
+const News = (props) => {
+
+    const [articles, setArticles] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState(1);
+    const [totalResults, setTotalResults] = useState(0);
+
+    const capitaliseFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    const updateNews = async () => {
+        props.setProgress(10);
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+        setLoading(true);
+        let data = await fetch(url);
+        props.setProgress(30);
+        let parsedData = await data.json();
+        props.setProgress(70);
+        setArticles(parsedData.articles);
+        setTotalResults(parsedData.totalResults);
+        setLoading(false);
+        props.setProgress(100);
+    }
+
+    useEffect(() => {
+        document.title = `Newsify - ${capitaliseFirstLetter(props.category)}`;
+        updateNews();
+        // eslint-disable-next-line
+    }, [])
+    // the above comment will disable the warning coming for the empty [] used in second parameter of useEffect as below
+
+    const fetchMoreData = async () => {
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
+        setPage(page + 1);                         // this takes time and so it is kept below url and page has been changed to page + 1 in url otherwise in this time the url would take the unupdated page only
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        setArticles(articles.concat(parsedData.articles));
+        setTotalResults(parsedData.totalResults);
+    };
+
+        return (
+            <>
+                <h1 className="text-center" style={{ margin: '35px', marginTop: '90px'}}>Newsify - Top {capitaliseFirstLetter(props.category)} Headlines</h1>
+                {loading && <Spinner/>}
+                <InfiniteScroll
+                    dataLength={articles.length}
+                    next={fetchMoreData}
+                    hasMore={articles.length !== totalResults}
+                    loader={<Spinner/>}
+                >
+                    <div className="container my-3">
+                        <div className='row'>
+                            {articles.map((element) => {          // iteration over the articles array
+                                return <div className="col-md-4" key={element.url}>
+                                    {/* key has to be something unique in every article  */}
+                                    <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+                                </div>
+                            })}
+                        </div>
+                    </div>
+                </InfiniteScroll>
+            </>
+        )
+}
+
+News.defaultProps = {
+    country: 'in',
+    pageSize: 6,
+    category: 'general'
+}
+
+News.propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string
+}
+
+export default News
